7fd8ba69fadfe12066d6134ee8eabedcc006d2ea
CristCT <crist.fract@gmail.com>
2022-10-24 01:10:28 -0300

    Update size img header popup

--- client/src/assets/Projects/index.js
+++ client/src/assets/Projects/index.js
@@ -1,11 +1,10 @@
-import imgUno0 from './PaginaWebTiendaDeJuegos/portada0.png';
+import imgUno0 from './PaginaWebTiendaDeJuegos/portada.png';
 import imgUno1 from './PaginaWebTiendaDeJuegos/portadaGameMania.png';
 import imgUno2 from './PaginaWebTiendaDeJuegos/formularioLogin.png';
 import imgUno3 from './PaginaWebTiendaDeJuegos/secciónRegistrar.png';
 import imgUno4 from './PaginaWebTiendaDeJuegos/secciónConsultar.png';
-import imgUno5 from './PaginaWebTiendaDeJuegos/portada.png';
 
-const imagesUno = [imgUno0, imgUno1, imgUno2, imgUno3, imgUno4, imgUno5];
+const imagesUno = [imgUno0, imgUno1, imgUno2, imgUno3, imgUno4, ];
 
 export default imagesUno;
 
