3d6e986b640f6e32b6f4d77b20ad1087b3bcc392
CristCT <crist.fract@gmail.com>
2023-01-07 04:31:03 -0300

    fix implemented functions

--- client/src/PortfolioContainer/GoTop/GoTop.js
+++ client/src/PortfolioContainer/GoTop/GoTop.js
@@ -5,9 +5,40 @@ import { AnimatePresence, motion } from "framer-motion";
 
 const GoTop = ({ closePopup }) => {
     const [hasScrolled, setHasScrolled] = useState(false);
+    // 
+    // Establece el umbral de desplazamiento en 50px para pantalla ancha
+    // y 20px para pantalla estrecha
+    let scrollThreshold = 50;
+
+    // Utiliza media queries para ajustar el umbral de desplazamiento
+    // dependiendo del ancho de la pantalla
+    if (matchMedia) {
+        const mq = window.matchMedia("(max-width: 600px)");
+        mq.addEventListener("change", WidthChange);
+        WidthChange(mq);
+    }
+
+    // Esta función se llama cada vez que el ancho de la pantalla cambia
+    function WidthChange(mq) {
+        if (mq.matches) {
+            // Pantalla estrecha: establece el umbral en 20px
+            scrollThreshold = 20;
+        } else {
+            // Pantalla ancha: establece el umbral en 50px
+            scrollThreshold = 50;
+        }
+        if (matchMedia) {
+            const mq320 = window.matchMedia("(max-width: 320px)");
+            if (mq320.matches) {
+                // Pantalla de 320px o menor: establece el umbral en 10px
+                scrollThreshold = 70;
+            } 
+        }
+    }
+
     // Check if the scroll has been done and change the state accordingly
     window.onscroll = function() {
-        if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 20) {
+        if (document.body.scrollTop > scrollThreshold || document.documentElement.scrollTop > scrollThreshold) {
             setHasScrolled(true);
         } else {
             setHasScrolled(false);
